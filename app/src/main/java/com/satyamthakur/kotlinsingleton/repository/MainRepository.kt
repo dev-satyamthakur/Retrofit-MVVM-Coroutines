837c54f390bd45204088ba0770581ddd0346f313
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-14 21:29:51 +0530

    Setting up the MainRepository

--- app/src/main/java/com/satyamthakur/kotlinsingleton/repository/MainRepository.kt
+++ app/src/main/java/com/satyamthakur/kotlinsingleton/repository/MainRepository.kt
@@ -1,5 +1,22 @@
 package com.satyamthakur.kotlinsingleton.repository
 
-object MainRepository {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.satyamthakur.kotlinsingleton.api.RetrofitService
+import com.satyamthakur.kotlinsingleton.models.QuoteResponse
+
+class MainRepository(private val quoteService: RetrofitService) {
+
+    private val quotesLiveData = MutableLiveData<QuoteResponse>()
+
+    val quotes: LiveData<QuoteResponse>
+        get() = quotesLiveData
+
+    suspend fun getQuotes(page: Int) {
+        val result = quoteService.getQuotes(page)
+        if (result?.body() != null) {
+            quotesLiveData.postValue(result.body())
+        }
+    }
 
 }

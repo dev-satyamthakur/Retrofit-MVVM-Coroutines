d3a95b4fcf1a6281e5cad0dd95d1c1020bd3dc50
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-14 21:41:23 +0530

    Setting up everything in MainActivity

--- app/src/main/java/com/satyamthakur/kotlinsingleton/MainActivity.kt
+++ app/src/main/java/com/satyamthakur/kotlinsingleton/MainActivity.kt
@@ -2,14 +2,31 @@ package com.satyamthakur.kotlinsingleton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.satyamthakur.kotlinsingleton.ViewModel.MainViewModel
+import com.satyamthakur.kotlinsingleton.ViewModel.MainViewModelFactory
+import com.satyamthakur.kotlinsingleton.api.MyRetrofitBuilder
+import com.satyamthakur.kotlinsingleton.api.RetrofitService
+import com.satyamthakur.kotlinsingleton.repository.MainRepository
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val quoteSer = MyRetrofitBuilder.getInstance().create(RetrofitService::class.java)
+        val repo = MainRepository(quoteSer)
+        mainViewModel = ViewModelProvider(this, MainViewModelFactory(repo))
+            .get(MainViewModel::class.java)
+
+        mainViewModel.quotes.observe(this, Observer {
+            Log.d("MYAPP", it.toString())
+        })
 
     }
 }

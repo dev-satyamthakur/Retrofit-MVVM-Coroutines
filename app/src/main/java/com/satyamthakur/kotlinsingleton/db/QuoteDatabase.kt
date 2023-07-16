e615d8678f239ea2da04dd096ddf05e66b1cc30f
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-16 16:44:02 +0530

    Creating database class for room

---
+++ app/src/main/java/com/satyamthakur/kotlinsingleton/db/QuoteDatabase.kt
@@ -0,0 +1,30 @@
+package com.satyamthakur.kotlinsingleton.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import kotlin.coroutines.coroutineContext
+
+@Database(entities = [Result::class], version = 1)
+abstract class QuoteDatabase: RoomDatabase() {
+
+    abstract fun getQuoteDao(): QuotesDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: QuoteDatabase? = null
+
+        fun getDatabase(context: Context): QuoteDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE = Room.databaseBuilder(context,
+                         QuoteDatabase::class.java, "quote_db")
+                        .build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+
+}
